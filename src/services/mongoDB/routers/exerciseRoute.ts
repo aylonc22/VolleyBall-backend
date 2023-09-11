55e5571baf1608c6bcd0ecb647765d58c202186e
Aylon <aylonc1@gmail.com>
2023-09-11 18:40:33 +0300

    getExercises HotFix

--- src/services/mongoDB/routers/exerciseRoute.ts
+++ src/services/mongoDB/routers/exerciseRoute.ts
@@ -1,11 +1,11 @@
 import { authenticateToken } from "../../authToken";
-import { addExercise, editExercise } from "../controllers/exersice-ctrl";
+import { addExercise, editExercise, getExercises } from "../controllers/exersice-ctrl";
 import express from 'express'
 
 
 const router = express.Router();
 router.post('/addExercise',authenticateToken,addExercise);
 router.put('/editExercise',authenticateToken,editExercise);
-
+router.get('/getExercises',authenticateToken,getExercises);
 
 module.exports = router;
